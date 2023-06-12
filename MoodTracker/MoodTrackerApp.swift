cf49f11c288c0164d86da328c1507178c6dc01c7
Kittyheart20 <superkittenlover@gmail.com>
2023-06-11 20:46:39 -0500

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Arin Haynes on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
